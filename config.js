29ebf47d551be5ce9f5cfe70ed3db17e2043b335
arslvn93 <58179630+arslvn93@users.noreply.github.com>
2026-05-13 14:44:54 -0400

    Update Site

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
-window.config = {
+const config = {
   "property": {
     "streetAddress": "1386 Lincoln Drive",
     "city": "Port Coquitlam",
     "cityStateZip": "Port Coquitlam, BC  V3B7G6",
     "heroSubtitle": "Prepare to be transformed by this stunning Townhouse that represents the pinnacle of residential design. With 2 carefully curated bedrooms, 2.5 luxurious bathrooms, and an expansive 1536 sq ft floor plan, this home is more than just a living space – it's a lifestyle statement. Every inch has been thoughtfully crafted to provide an exceptional living experience that balances aesthetic appeal with practical functionality, creating a harmonious environment that adapts to your most discerning needs.",
-    "backgroundImageUrl": "https://salesgeniusportal.s3.ca-central-1.amazonaws.com/atlas-group/marketing-requests/open-house/1778621031514-8-web-or-mls-Photo-8.jpg"
+    "backgroundImageUrl": "https://salesgeniusportal.s3.ca-central-1.amazonaws.com/TeamUploads/upload-1386LincolnDrive1.jpg"
   },
   "realtor": {
     "fullName": "Liam Thomas",
@@ -16,7 +16,7 @@ window.config = {
     "social": {
       "facebook": "https://www.facebook.com/profile.php?id=61567311449154",
       "instagram": "https://www.instagram.com/therealatlasgroup",
-      "website": "#"
+      "website": ""
     }
   },
   "brokerage": {
@@ -27,22 +27,19 @@ window.config = {
   "openHouse": {
     "eventDate": "",
     "bundleItems": [
-            {
-                  "icon": "fas fa-home",
-                  "text": "Full Property Listing",
-                  "url": "https://www.realtor.ca/real-estate/29741701/135-1386-lincoln-drive-port-coquitlam"
-            },
-            {
-                  "icon": "fas fa-ruler-combined",
-                  "text": "Architectural Floor Plan",
-                  "url": "https://salesgeniusportal.s3.ca-central-1.amazonaws.com/atlas-group/marketing-requests/open-house/1778620404167-135 - 1386 LINCOLN DR Floor Plan 8.5 X 11.pdf"
-            },
-            {
-                  "icon": "fas fa-video",
-                  "text": "Video Tour",
-                  "url": "https://youtu.be/vYyeriwYWUA"
-            }
-      ]
+      {
+        "icon": "fas fa-home",
+        "text": "Full Property Listing"
+      },
+      {
+        "icon": "fas fa-ruler-combined",
+        "text": "Architectural Floor Plan"
+      },
+      {
+        "icon": "fas fa-video",
+        "text": "Video Tour"
+      }
+    ]
   },
   "meta": {
     "pageTitlePrefix": "🏡 Liam Thomas Open House",
@@ -50,7 +47,7 @@ window.config = {
     "privacyPolicyLink": "https://atlasgrouprealty.ca/site/privacy-terms"
   },
   "settings": {
-   "primaryColor": "#2a3825",
+    "primaryColor": "#2a3825",
     "secondaryColor": "#f2ca53",
     "accentColor": "",
     "neutralColor": "",
@@ -65,46 +62,91 @@ window.config = {
     ]
   },
   "modalQuestions": [
-  {
-    "id": "q_auto_1750445840887_0",
-    "questionText": "What's your current situation?",
-    "options": [
-      { "value": "I own my home", "text": "I own my home" },
-      { "value": "I rent", "text": "I rent" },
-      { "value": "Live with family/friends", "text": "Live with family/friends" },
-      { "value": "Between homes", "text": "Between homes" }
-    ]
-  },
-  {
-    "id": "q_auto_1750445840887_1",
-    "questionText": "What are you looking to do?",
-    "options": [
-      { "value": "Buy a home", "text": "Buy a home" },
-      { "value": "Sell and buy", "text": "Sell and buy" },
-      { "value": "Rent a place", "text": "Rent a place" },
-      { "value": "Invest in property", "text": "Invest in property" }
-    ]
-  },
-  {
-    "id": "q_auto_1750445840887_2",
-    "questionText": "When are you planning to move?",
-    "options": [
-      { "value": "ASAP", "text": "ASAP" },
-      { "value": "3-6 months", "text": "3-6 months" },
-      { "value": "6-12 months", "text": "6-12 months" },
-      { "value": "12+ months", "text": "12+ months" }
-    ]
-  },
-  {
-    "id": "agent_status",
-    "questionText": "Are you currently exploring homes with a real estate agent?",
-    "options": [
-      { "value": "committed", "text": "Yes, I'm committed to an agent." },
-      { "value": "not_committed", "text": "No, I'm not committed to an agent." },
-      { "value": "gathering_info", "text": "Just gathering information at this stage." }
-    ]
-  }
-],
+    {
+      "id": "q_auto_1750445840887_0",
+      "questionText": "What's your current situation?",
+      "options": [
+        {
+          "value": "I own my home",
+          "text": "I own my home"
+        },
+        {
+          "value": "I rent",
+          "text": "I rent"
+        },
+        {
+          "value": "Live with family/friends",
+          "text": "Live with family/friends"
+        },
+        {
+          "value": "Between homes",
+          "text": "Between homes"
+        }
+      ]
+    },
+    {
+      "id": "q_auto_1750445840887_1",
+      "questionText": "What are you looking to do?",
+      "options": [
+        {
+          "value": "Buy a home",
+          "text": "Buy a home"
+        },
+        {
+          "value": "Sell and buy",
+          "text": "Sell and buy"
+        },
+        {
+          "value": "Rent a place",
+          "text": "Rent a place"
+        },
+        {
+          "value": "Invest in property",
+          "text": "Invest in property"
+        }
+      ]
+    },
+    {
+      "id": "q_auto_1750445840887_2",
+      "questionText": "When are you planning to move?",
+      "options": [
+        {
+          "value": "ASAP",
+          "text": "ASAP"
+        },
+        {
+          "value": "3-6 months",
+          "text": "3-6 months"
+        },
+        {
+          "value": "6-12 months",
+          "text": "6-12 months"
+        },
+        {
+          "value": "12+ months",
+          "text": "12+ months"
+        }
+      ]
+    },
+    {
+      "id": "agent_status",
+      "questionText": "Are you currently exploring homes with a real estate agent?",
+      "options": [
+        {
+          "value": "committed",
+          "text": "Yes, I'm committed to an agent."
+        },
+        {
+          "value": "not_committed",
+          "text": "No, I'm not committed to an agent."
+        },
+        {
+          "value": "gathering_info",
+          "text": "Just gathering information at this stage."
+        }
+      ]
+    }
+  ],
   "deploymentInfo": {
     "repoId": "1237083153",
     "repoName": "Open-House-945",
