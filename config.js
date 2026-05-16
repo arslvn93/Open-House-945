a50a2b71792e8907210c5cc23f802d491da8886c
arslvn93 <58179630+arslvn93@users.noreply.github.com>
2026-05-16 09:40:42 -0400

    Update Site

--- config.js
+++ config.js
@@ -152,7 +152,7 @@ const config = {
     "repoName": "Open-House-945",
     "crmLeadParsingEmail": "",
     "tag": "Open House 1386 Lincoln Drive",
-    "netlifyUrl": "https://services.leadconnectorhq.com/hooks/6e1vj0zCa5IdQTgIJulF/webhook-trigger/5c8bfd3d-9639-4d4f-ad51-662978f3811f",
+    "netlifyUrl": "https://services.leadconnectorhq.com/hooks/me7tB96AKcd36NuvyMuJ/webhook-trigger/RaqKrgGdcVy68t8Yd1vY",
     "sgApiKey": "api_e66852530c4898db50c8c0f4f8dc764e"
   }
 };
