6d5757e2f905afff43f034a1b848abe987988827
arslvn93 <58179630+arslvn93@users.noreply.github.com>
2026-05-16 09:31:48 -0400

    Update Site

--- config.js
+++ config.js
@@ -153,6 +153,6 @@ const config = {
     "crmLeadParsingEmail": "",
     "tag": "Open House 1386 Lincoln Drive",
     "netlifyUrl": "https://services.leadconnectorhq.com/hooks/6e1vj0zCa5IdQTgIJulF/webhook-trigger/1612e579-5cf9-4946-b7e7-d3bdaac447b0",
-    "sgApiKey": "api_e66852530c4898db50c8c0f4f8dc764e"
+    "sgApiKey": ""
   }
 };
